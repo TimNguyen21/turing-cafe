97d3e6fb7d97622ee65eef493ca8e43e7df7fb53
Tim Nguyen <tim.nguyen2112@gmail.com>
2020-04-02 11:31:59 -0600

    Add testing for App component

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { render, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+describe("App", () => {
+  it("should display new Reservation on submit", () => {
+    const { getByText, getByPlaceholderText } = render(<App />)
+
+    fireEvent.change(getByPlaceholderText("name"), {target: {value: "Tim"}});
+    fireEvent.change(getByPlaceholderText("date (mm/dd)"), {target: {value: '01/01'}});
+    fireEvent.change(getByPlaceholderText("time (hh:mm)"), {target: {value: "12:15"}});
+    fireEvent.change(getByPlaceholderText("number of guests"), {target: {value: 60}});
+
+    fireEvent.click(getByText("Make Reservations"));
+
+    expect(getByText("Tim")).toBeInTheDocument();
+    expect(getByText("01/01")).toBeInTheDocument();
+    expect(getByText("12:15")).toBeInTheDocument();
+    expect(getByText("Number of Guest: 60")).toBeInTheDocument();
+  })
+})
